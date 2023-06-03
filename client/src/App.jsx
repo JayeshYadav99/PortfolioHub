fe41f740d2f2a7001d71715f2feb182b07e2a02b
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-06-03 15:41:09 +0530

    Profile Update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,7 +7,7 @@ import Home from "./Pages/Home";
 import Navbar from "./Pages/Navbar";
 import Hero from "./Pages/Hero";
 import Footer from "./Pages/Footer";
-
+import Profoliotemp from "./Pages/Portfolio/Profoliotemp";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import PortfolioHubHome from "./Pages/PortfolioHubHome";
 
@@ -26,6 +26,7 @@ const App = () => {
             <Route exact path="/Form" element={<Form />}></Route>
 
             <Route exact path="/AUth" element={<Auth />}></Route>
+            <Route exact path="/Portfolio" element={<Profoliotemp />}></Route>
           </Routes>
 
           <Footer />
