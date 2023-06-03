b27bf48f5903ef95b3d22f8ecbdca8f2e4bdd548
Jayesh Yadav <jayeshyadav528@gmail.com>
2023-06-04 01:49:27 +0530

    Update About.jsx

--- client/src/Pages/Portfolio/About.jsx
+++ client/src/Pages/Portfolio/About.jsx
@@ -10,13 +10,12 @@ export default function About() {
             February 2022
           </time>
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            Application UI code in Tailwind CSS
+          Engineering 
           </h3>
           <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
-            Get access to over 20+ pages including a dashboard layout, charts,
-            kanban board, calendar, and pre-order E-commerce & Marketing pages.
+          The power of first impressions cannot be underestimated, and the gateway to capitalizing on them lies in exceptional website design. An outstanding website transcends mere aesthetics and extends its influence to encompass seamless functionality and user-friendly navigation. Drawing upon my expertise as a seasoned programmer, I possess the unique ability to tackle intricate technical challenges while crafting websites that exude sleekness and visual allure. Moreover, my extensive knowledge of recognized technical standards is complemented by my proficiency in modern building practices, ensuring that every aspect of your website is finely tuned to perfection.
           </p>
-          <a
+          {/* <a
             href="#"
             className="inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-200 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
           >
@@ -33,7 +32,7 @@ export default function About() {
                 clip-rule="evenodd"
               ></path>
             </svg>
-          </a>
+          </a> */}
         </li>
         <li className="mb-10 ml-4">
           <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
@@ -41,12 +40,10 @@ export default function About() {
             March 2022
           </time>
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            Marketing UI design in Figma
+          Product 
           </h3>
           <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            All of the pages and components are first designed in Figma and we
-            keep a parity between the two versions even as we update the
-            project.
+          While I may not fit the conventional mold of a product manager, my diverse skill set in research, product design, and product coordination empowers me to drive the growth of a product from its inception. As an exceptional analytical thinker, I possess the ability to uphold the product's vision throughout its entire journey, effectively bridging the technical and product aspects. By leveraging my expertise, I can navigate the path from 0 to 1, ensuring the product's success at every stage.
           </p>
         </li>
         <li className="ml-4">
@@ -55,11 +52,10 @@ export default function About() {
             April 2022
           </time>
           <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-            E-Commerce UI code in Tailwind CSS
+          Design 
           </h3>
           <p className="text-base font-normal text-gray-500 dark:text-gray-400">
-            Get started with dozens of web components and interactive elements
-            built on top of Tailwind CSS.
+          Despite not fitting the typical designer stereotype, my exceptional visual abilities enable me to excel as a presenter, effectively conveying design concepts to stakeholders and design teams. I possess a remarkable aptitude for conceptualization, allowing me to envision and bring forth stunning creations. Additionally, my expertise lies in fine-tuning stylesheets and crafting seamless user experiences that flow effortlessly.
           </p>
         </li>
       </ol>
