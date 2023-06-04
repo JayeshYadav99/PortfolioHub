929e54c6cba5a35488827c612641c1173c12a90c
Nishitbariya <85815172+Nishitbaria@users.noreply.github.com>
2023-06-04 17:37:09 +0530

    Update Certification.jsx

--- client/src/Pages/Certification.jsx
+++ client/src/Pages/Certification.jsx
@@ -6,9 +6,9 @@ function Certification() {
       "url('//s3-us-west-2.amazonaws.com/s.cdpn.io/4273/frame.png') 93 92 87 92 stretch stretch",
     borderColor: "#f4be52",
     borderStyle: "inset",
-    borderWidth: "60px",
+    borderWidth: "40px",
     display: "block",
-    width: "40%",
+    width: "50%", // Adjust the width here
     height: "auto",
     backgroundColor: "#ffe",
     margin: "0 auto",
@@ -20,24 +20,30 @@ function Certification() {
   };
   return (
     <>
-      <div className="flex flex-col w-full">
+      <h2
+        className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white"
+        style={{ textAlign: "center" }}
+      >
+        Certification Section
+      </h2>
+      <div className="flex flex-row w-full">
         <div className="my-16">
           <img
-            src="https://cdn-icons-png.flaticon.com/512/3476/3476457.png"
+            src="https://images.credly.com/size/680x680/images/6240e108-1407-4773-8621-cc2e4736d4e6/Web_Development_with_HTML-CSS-JavaScript_Essentials.png "
             alt="Example"
             style={frameStyle}
           />
         </div>
         <div className="my-16">
           <img
-            src="https://cdn-icons-png.flaticon.com/512/3476/3476457.png"
+            src="https://media.licdn.com/dms/image/C4D1FAQGZVpx-Nx5Hmg/feedshare-document-cover-images_1280/0/1679155504682?e=1686484800&v=beta&t=Tk5f1EGPJhX3stnur43cGUqJJtrZnLZwOeTAatTkwlE"
             alt="Example"
             style={frameStyle}
           />
         </div>
         <div className="my-16">
           <img
-            src="https://cdn-icons-png.flaticon.com/512/3476/3476457.png"
+            src="https://images.credly.com/images/23859131-d0ff-4f44-900f-bac86165b941/image.png"
             alt="Example"
             style={frameStyle}
           />
